478f0c7185ac2ce5888eeebee49e3043470c1cab
Nathan L <nathan.lachance@gmail.com>
2021-05-18 13:53:33 -0400

    Added the code to select only the columns with mean and std and create a column with the activity names

--- run_analysis.R
+++ run_analysis.R
@@ -26,3 +26,22 @@ if(!exists("merged")){ #merging the test and train data
     rbind(testmerged,trainmerged) -> merged
 }
 
+#locations with names from features.txt with mean() in the name
+meanlocs <- c(1,2,3,41,42,43,81,82,83,121,122,123,161,162,163,201,202,203,214,227,240,253,266,267,268,345,346,347,424,425,426,503,513,516,529,542)
+#locations with names from features.txt with std() in the name
+stdlocs <- c(4,5,6,44,45,46,84,85,86,124,125,126,164,165,166,202,215,228,241,254,269,270,271,348,349,350,427,428,429,504,517,530,543)
+features <- read.table("./UCI HAR Dataset/features.txt")
+
+c(meanlocs,stdlocs) -> mergedlocs
+# added 1 and 2 so they are kept in the end dataset along with all the
+# columns for std and mean which have been shifted over by 2 places due
+# to the addition of y and subjects
+c(1,2,mergedlocs+2) -> mergedlocs
+
+# A dataset containing only the labels and columns with mean or std
+merged[,mergedlocs] -> selected
+
+# Setting up a vector to use to index names
+activityLabels <- c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING,STANDING","LAYING")
+# Creates a column called "activityname" to merge the data
+selected$activityname <- activityLabels[selected$V1.1]
