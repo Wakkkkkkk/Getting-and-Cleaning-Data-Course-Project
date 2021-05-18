40677cbd0d79339fb2140b9a1b2e62d0a475ae3b
Nathan L <nathan.lachance@gmail.com>
2021-05-18 12:07:30 -0400

    added a check to see if the data is already loaded into R to allow tests to go faster

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,13 @@
-#loading the data into R
-#test
-read.table("./UCI HAR Dataset/test/X_test.txt")->testdat
-read.table("./UCI HAR Dataset/test/subject_test.txt")->testsub
-read.table("./UCI HAR Dataset/test/y_test.txt")->ytest
-
-#train
-read.table("./UCI HAR Dataset/train/X_train.txt")->traindat
-read.table("./UCI HAR Dataset/train/subject_train.txt")->trainsub
-read.table("./UCI HAR Dataset/train/y_train.txt")->ytrain
+#checking if the data is already loaded to allow faster testing
+if(!exists("testdat")){
+    #loading the data into R
+    #test
+    read.table("./UCI HAR Dataset/test/X_test.txt")->testdat
+    read.table("./UCI HAR Dataset/test/subject_test.txt")->testsub
+    read.table("./UCI HAR Dataset/test/y_test.txt")->ytest
+    
+    #train
+    read.table("./UCI HAR Dataset/train/X_train.txt")->traindat
+    read.table("./UCI HAR Dataset/train/subject_train.txt")->trainsub
+    read.table("./UCI HAR Dataset/train/y_train.txt")->ytrain
+}
