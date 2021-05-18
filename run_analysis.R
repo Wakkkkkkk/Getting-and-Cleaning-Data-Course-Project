4387cc331526681f14cc5ef7d16ee3f14802652c
Nathan L <nathan.lachance@gmail.com>
2021-05-18 12:46:38 -0400

    Added code to merge the test and train data

--- run_analysis.R
+++ run_analysis.R
@@ -4,10 +4,25 @@ if(!exists("testdat")){
     #test
     read.table("./UCI HAR Dataset/test/X_test.txt")->testdat
     read.table("./UCI HAR Dataset/test/subject_test.txt")->testsub
-    read.table("./UCI HAR Dataset/test/y_test.txt")->ytest
+    read.table("./UCI HAR Dataset/test/y_test.txt")->testy
     
     #train
     read.table("./UCI HAR Dataset/train/X_train.txt")->traindat
     read.table("./UCI HAR Dataset/train/subject_train.txt")->trainsub
-    read.table("./UCI HAR Dataset/train/y_train.txt")->ytrain
+    read.table("./UCI HAR Dataset/train/y_train.txt")->trainy
 }
+
+if(!exists("testmerged")){ #merging the test data
+    cbind(testsub,testy)->testmerged
+    cbind(testmerged,testdat)->testmerged
+}
+
+if(!exists("trainmerged")){ #merging the train data
+    cbind(trainsub,trainy)->trainmerged
+    cbind(trainmerged,traindat)->trainmerged
+}
+
+if(!exists("merged")){ #merging the test and train data
+    rbind(testmerged,trainmerged) -> merged
+}
+
